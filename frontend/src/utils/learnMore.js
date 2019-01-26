08b007712e0fdb60fb5073124422fb6704a76f80
Dhvanil Shah <dhvanil.shah98@gmail.com>
2019-01-25 23:01:33 -0500

    reformatted website for Salicornia

--- frontend/src/utils/learnMore.js
+++ frontend/src/utils/learnMore.js
@@ -36,21 +36,23 @@ export default class LearnMore extends React.Component {
           onCancel={this.handleCancel}
         >
           <p>
-            This project is a collaboration between students within the
-            Electrical, Chemical, and General Engineering disciplines in efforts
-            to design and implement a hydroponics system capable of growing
-            Salicornia Bigellovi and converting the grown biomass into biojet
-            fuel. In our current stage, we are developing an automated
-            hydroponics system capable of (1) controlling the plant’s light,
-            nutrient, and environmental conditions (2) aggregating sensory and
-            harvest data during the plant’s growth cycle. This system will be
-            capable of being interfaced and monitored remotely via a dedicated
-            website. The goal of this stage is to be able to use the aggregated
-            data to create a "growth recipe” which can be inputted to the
-            automated hydroponics system to grow Salicornia quickly and
-            efficiently. Salicornia is chosen because it is an inedible
-            halophyte, meaning that it does not require freshwater to grow and
-            therefore will not disrupt the food ecosystem.
+            We are working on designing and implementing a hydroponics system
+            capable of growing Salicornia Bigellovi and converting the grown
+            biomass into biojet fuel. Students in the Electrical, Chemical, and
+            General Engineering disciplines are collaborating with Professor
+            Davis and Professor Shlayan to build out a hydroponics system to
+            optimize the growth of Salicornia. In our current stage, we are
+            developing an automated hydroponics system capable of (1)
+            controlling the plant’s light, nutrient, and environmental
+            conditions (2) aggregating sensory and harvest data during the
+            plant’s growth cycle. This system will be able to be interfaced with
+            a dedicated website to allow for remote monitoring. The goal of this
+            current stage is to be able to use the aggregated data to create a
+            "growth recipe” which can be input to the automated hydroponics
+            system to allow for the Salicornia to grow quickly and efficiently.
+            We are using Salicornia because it is an inedible halophyte, meaning
+            that it does not require freshwater to grow and therefore will not
+            disrupt the food ecosystem.
           </p>
         </Modal>
       </div>
